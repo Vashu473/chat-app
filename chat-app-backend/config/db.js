8067d7c558155ae1c2e246fa8b728f71fc02240a
Vashudev <vmanishkumar04@gmail.com>
2023-09-22 17:24:40 +0530

    new changfes'

--- chat-app-backend/config/db.js
+++ chat-app-backend/config/db.js
@@ -4,11 +4,14 @@ const colors = require("colors");
 const connectDB = async () => {
   try {
     // for your real project use .env file for db urls
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/chat", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: true,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://vmanishkumar04:vashudev@cluster0.8n7bdot.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+      }
+    );
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
